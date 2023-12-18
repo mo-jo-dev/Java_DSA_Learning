97798a907254052bc27007b6096a9756a692bc0d
Mohit Joshi <b210055@nitsikkim.ac.in>
2023-12-18 23:40:38 +0530

    added 1st program

---
+++ 01 - First Program/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){    //int main() in java is like this
+        // System.out.println("Hello World!");    //printf or cout of java
+        System.out.println(args[1]);
+    }
+}
