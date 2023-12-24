1f48f61e33e35d732b3e5a979d12a515399d6b03
Mohit Joshi <b210055@nitsikkim.ac.in>
2023-12-24 11:29:11 +0530

    Added main program

---
+++ 05 - Arrays/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print(`Namaste World`);
+    }
+}
