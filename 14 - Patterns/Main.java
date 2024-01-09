3b7d0357750e7be85921d0018019a611aefcdee4
Mohit Joshi <b210055@nitsikkim.ac.in>
2024-01-09 11:49:22 +0530

    Pattern 4

--- 14 - Patterns/Main.java
+++ 14 - Patterns/Main.java
@@ -8,7 +8,13 @@ public class Main
         // No. of lines = No. of rows = No. of lines outer loop will run
 
         pattern1(4);
+        System.out.println();
         pattern2(4);
+        System.out.println();
+        pattern3(4);
+        System.out.println();
+        pattern4(4);
+        System.out.println();
     }
 
     static void pattern1(int n){
@@ -24,6 +30,24 @@ public class Main
         for(int row = 1; row <= n; row++){
             // For every row, run the col
             for(int col = 1; col <= row; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n){
+        for(int row = 1; row <= n; row++){
+            for(int col = n-row + 1; col >= 1; col--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n){
+        for(int row = 1; row <= n; row++){
+            for(int col = n-row + 1; col >= 1; col--){
                 System.out.print("* ");
             }
             System.out.println();
