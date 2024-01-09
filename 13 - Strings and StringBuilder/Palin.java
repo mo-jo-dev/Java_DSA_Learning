05984b43927be7d102ec6fdfd2a7b0b217bb911f
Mohit Joshi <b210055@nitsikkim.ac.in>
2024-01-09 11:23:31 +0530

    Palindrome checker

--- 13 - Strings and StringBuilder/Palin.java
+++ 13 - Strings and StringBuilder/Palin.java
@@ -10,6 +10,9 @@ public class Palin
     }
 
     static boolean isPalindrome(String str){
+        if(str == null || str.length() == 0){
+            return true;
+        }
         str = str.toLowerCase();
         for(int i = 0; i < str.length()/2; i++){
             char start = str.charAt(i);
