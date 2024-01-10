768fcf807f43adbb61ec9cb0eb6afbb386e399c4
Mohit Joshi <b210055@nitsikkim.ac.in>
2024-01-10 10:03:51 +0530

    Added pattern 5

--- 14 - Patterns/Main.java
+++ 14 - Patterns/Main.java
@@ -15,6 +15,8 @@ public class Main
         System.out.println();
         pattern4(4);
         System.out.println();
+        pattern5(5);
+        System.out.println();
     }
 
     static void pattern1(int n){
@@ -49,6 +51,16 @@ public class Main
         for(int row = 1; row <= n; row++){
             for(int col = 1; col <= row; col++){
                 System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n){
+        for(int row = 0; row < 2*n; row++){
+            int totalColsInRow = (row < n) ? row : 2*n - row;
+            for(int col = 0; col < totalColsInRow; col++){
+                System.out.print("* ");
             }
             System.out.println();
         }
