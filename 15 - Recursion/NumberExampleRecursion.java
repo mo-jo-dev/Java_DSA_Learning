d032ab93a6ea9294534ae5042c45208df5dc57c1
Mohit Joshi <b210055@nitsikkim.ac.in>
2024-01-11 16:05:13 +0530

    added recursive program for number exchange

---
+++ 15 - Recursion/NumberExampleRecursion.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class NumberExampleRecursion
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        print(1);
+    }
+
+    static void print(int n){
+
+        System.out.println(n);
+        if(n == 5) {
+            return;
+            }
+        print(n+1);
+    }
+}
