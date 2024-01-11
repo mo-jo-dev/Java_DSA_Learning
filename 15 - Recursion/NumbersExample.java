c907de9c8c3e2f4b2286617cf129a7648a998bb0
Mohit Joshi <b210055@nitsikkim.ac.in>
2024-01-11 12:29:24 +0530

    Number Example withour recursion

---
+++ 15 - Recursion/NumbersExample.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class NumbersExample
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        print1(1);
+    }
+
+    static void print1(int n){
+        System.out.println(n);
+        print2(2);
+    }
+
+    static void print2(int n){
+        System.out.println(n);
+        print3(3);
+    }
+
+    static void print3(int n){
+        System.out.println(n);
+        print4(4);
+    }
+
+    static void print4(int n){
+        System.out.println(n);
+        print5(5);
+    }
+
+    static void print5(int n){
+        System.out.println(n);
+    }
+}
