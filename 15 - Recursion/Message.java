371e4391dff6f6b4011d87f46edaa643ea00d875
Mohit Joshi <b210055@nitsikkim.ac.in>
2024-01-11 12:03:46 +0530

    Added message program

---
+++ 15 - Recursion/Message.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Message
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        // System.out.print();
+        message();
+    }
+
+    static void message(){
+        System.out.println("Hello World");
+        message1();
+    }
+
+    static void message1(){
+        System.out.println("Hello World");
+        message2();
+    }
+
+    static void message2(){
+        System.out.println("Hello World");
+        message3();
+    }
+
+    static void message3(){
+        System.out.println("Hello World");
+        message4();
+    }
+
+    static void message4(){
+        System.out.println("Hello World");
+    }
+}
